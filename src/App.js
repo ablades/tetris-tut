b56d93f91a816d1637bd021c45d9395ce17442f1
ablades <audaris@gmail.com>
2020-10-28 12:00:42 -0500

    add message popup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import GridBoard from './components/GridBoard'
 import NextBlock from './components/NextBlock'
 import ScoreBoard from './components/ScoreBoard'
 import Controls from './components/Controls'
+import MessagePopup from './components/MessagePopup'
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +16,9 @@ function App() {
       <NextBlock />
       <ScoreBoard />
       <Controls />
+      <MessagePopup />
     </div>
-  )
+  );
 }
+
+export default App
